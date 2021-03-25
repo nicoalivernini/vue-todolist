052b826e4550b95fc553cbe48e42d8b88fd59cea
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-25 16:01:05 +0100

    Creazione base to do list

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,24 @@ var app = new Vue (
   {
     el: '#root',
     data: {
+      toDos: [
+        {
+          title: 'Allenarsi',
+          scope: 'to-do'
+        },
+        {
+          title: 'Bere acqua',
+          scope: 'to-do'
+        },
+        {
+          title: 'Fare la spesa',
+          scope: 'to-do'
+        },
+        {
+          title: 'Esercizi Inglese',
+          scope: 'to-do'
+        }
+      ]
 
     },
 
@@ -11,3 +29,9 @@ var app = new Vue (
 
 
 }); //Chiusura Vue
+
+//Dobbiamo fare un'array di cose da fare così da avere alcuni esempio
+//Dobbiamo dare la possibilità di aggiungere le cose da poter fare
+  //Calcolare invio con la tastiera
+  //L'inserimento di stringa vuota da disabilitare
+  //Far tornare l'input vuoto quando mando il comando per inserire
